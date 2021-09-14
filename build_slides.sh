ff83f3ed764861dd02aa3f1e476fb83187c676c9
Colin Gross <grosscol@gmail.com>
2021-09-13 21:01:22 -0400

    Add convenience script for building slides.

---
+++ build_slides.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# Run from project root directory to create index.html
+pandoc -t revealjs --template=pandoc-templates/default.revealjs \
+  -o index.html  --slide-level=2 \
+  -V reveal.js=./reveal.js/dist/reveal.js  -V theme=serif slides/*.md
+
+echo "index.html built $(date)"
+
