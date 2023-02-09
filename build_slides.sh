8f1c431b3b0c866a464972762e7a76df34540452
Colin Gross <grosscol@gmail.com>
2023-02-09 10:59:49 -0500

    Hack reveal styles without making a theme.

--- build_slides.sh
+++ build_slides.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Run from project root directory to create index.html
-pandoc -t revealjs --template=pandoc-templates/default.revealjs \
+pandoc -t revealjs --template=modified.revealjs \
   -o index.html  --slide-level=2 \
   -V reveal.js=./reveal.js/dist/reveal.js  -V theme=serif slides/*.md
 
